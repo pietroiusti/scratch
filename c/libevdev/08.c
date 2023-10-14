2dff01c4bbc3b4e7617ddeb34e32fe1dd01581e9
gp <giulio.pietroiusti@gmail.com>
2023-10-14 17:14:01 +0200

    Fix loops.
    Some loops were using size_t for the loop variable i, and we were
    decrementing it in each iteration. When i reaches 0, and we decrement
    it, it wraps around to a large positive value instead of becoming
    negative...

--- c/libevdev/08.c
+++ c/libevdev/08.c
@@ -49,6 +49,7 @@
 #include <linux/input.h>
 #include <stddef.h> // ??
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -338,7 +339,7 @@ static void set_selected_key_maps() {
 //
 // loop backwards
 unsigned first_fun(unsigned code) {
-  for (int i = selected_key_maps_size-1; i >= 0; i--) {
+  for (size_t i = selected_key_maps_size-1; i != SIZE_MAX; i--) {
     if ((selected_key_maps[i]->key_from == code
          &&
          !selected_key_maps[i]->mod_from)
@@ -473,7 +474,7 @@ unsigned is_logically_down(unsigned code) {
     }
   }
 
-  for (size_t i = selected_key_maps_size-1; i >= 0; i--) {
+  for (size_t i = selected_key_maps_size-1; i != SIZE_MAX; i--) {
     if (selected_key_maps[i]->key_to == code
         && selected_key_maps[i]->key_from
         && !selected_key_maps[i]->mod_from
@@ -609,7 +610,7 @@ static key_map* is_key_in_uniquely_active_combo_map(unsigned code) {
   // let's loop backwards so we just take the first match if any
   // (because the non-default window map, whose key maps have
   // precedence, comes later, if present)
-  for(size_t i = selected_key_maps_size-1; i >= 0; i--) {
+  for(size_t i = selected_key_maps_size-1; i != SIZE_MAX; i--) {
 
     if (selected_key_maps[i]->key_from == first_fun(code)) {
 
@@ -639,7 +640,7 @@ static key_map* is_key_in_uniquely_active_combo_map(unsigned code) {
 // analogously to is_key_in_uniquely_active_combo_map
 static key_map* is_mod_in_uniquely_active_combo_map(unsigned code) {
 
-  for (size_t i = selected_key_maps_size-1; i >= 0; i--) {
+  for (size_t i = selected_key_maps_size-1; i != SIZE_MAX; i--) {
 
     if (selected_key_maps[i]->mod_from == first_fun(code)) {
 
