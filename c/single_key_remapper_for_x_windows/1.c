0f1d05f567433b5852c9e389bb70fa4213b4607b
gp <giulio.pietroiusti@gmail.com>
2023-11-10 23:09:02 +0100

    Create single key rempper 1.c file.

---
+++ c/single_key_remapper_for_x_windows/1.c
@@ -0,0 +1,22 @@
+/**
+   x windows key single key remapper.
+
+   Giulio Pietroiusti. 2023.
+
+   This program allows to remap single keys to other single keys when
+   the name of currently focused X window matches one of some strings
+   of our choice.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
