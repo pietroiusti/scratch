dc5b6858f83cfe6679dd54114e2df6cb75e0e568
gp <giulio.pietroiusti@gmail.com>
2023-11-19 23:43:06 +0100

    Print window names.
    Print window names when switcthing window. TODO: print name of window
    we are in when starting the program.

--- c/single_key_remapper_for_x_windows/1.c
+++ c/single_key_remapper_for_x_windows/1.c
@@ -21,12 +21,23 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+
+/**
+   Compile with:
+   gcc -pthread -g `pkg-config --libs x11` ./01.c
+*/
+
 #include <linux/input-event-codes.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+#include <pthread.h>
 
 typedef struct {
   unsigned key_from;
@@ -34,8 +45,8 @@ typedef struct {
 } key_map;
 
 typedef struct {
-  char* window_class_name;
-  key_map* key_maps;
+  char *window_class_name;
+  key_map *key_maps;
   unsigned key_maps_size;
 } window_map;
 
@@ -53,7 +64,72 @@ window_map window_maps[] = {
   4 }
 };
 
+void *track_window() {
+  Display* display;
+  XEvent xevent;
+  char *display_name = getenv("DISPLAY");
+  display = XOpenDisplay(display_name);
+  if (display == NULL) {
+    printf("display null\n");
+    exit(1);
+  }
+  Window root_window = DefaultRootWindow(display);
+  Atom active_window_atom = XInternAtom(display, "_NET_ACTIVE_WINDOW", False);
+  XSelectInput(display, root_window, PropertyChangeMask);
+  // return values
+  Atom type_return;
+  int format_return;
+  unsigned long nitems_return;
+  unsigned long bytes_left;
+  unsigned char *data;
+
+  // Get name of the focused window window when focus changes
+  while (1) {
+    XNextEvent(display, &xevent);
+
+    if (xevent.xproperty.atom != active_window_atom)
+      continue;
+
+    XGetWindowProperty(display,
+                       root_window,
+                       active_window_atom,
+                       0,
+                       1,
+                       False,
+                       XA_WINDOW,
+                       &type_return,   //should be XA_WINDOW
+                       &format_return, //should be 32
+                       &nitems_return, //should be 1 (zero if there is no such window)
+                       &bytes_left,    //should be 0 (i'm not sure but should be atomic read)
+                       &data           //should be non-null
+		       );
+
+    Window focused_window = *(Window *)data;
+
+    if (focused_window == 0)
+      continue;
+
+    char* window_name1;
+    if (XFetchName(display, focused_window, &window_name1) != 0) {
+      printf("The active window is: %s\n", window_name1);
+      XFree(window_name1);
+    }
+    XClassHint class_hint;
+    if (XGetClassHint(display, focused_window, &class_hint) == 0)
+      continue;
+    char *window_class = class_hint.res_class;
+    char *window_name2 = class_hint.res_name;
+    printf("res.class = %s\n", window_class);
+    printf("res.name = %s\n", window_name2);
+    printf("\n\n");
+
+    //set_currently_focused_window(window_class);
+  }
+}
+
 int main(void) {
+  printf("Initializing...\n");
+
   for (size_t i = 0; i < sizeof(window_maps)/sizeof(window_maps[0]); i++) {
     printf("The %s map has %u key maps:\n", window_maps[i].window_class_name, window_maps[i].key_maps_size);
 
@@ -62,7 +138,13 @@ int main(void) {
     }
   }
 
-  printf("Done. :)\n");
+  pthread_t track_window_thread;
+  int track_window_thread_return_value;
+  track_window_thread_return_value = pthread_create(&track_window_thread, NULL, track_window, NULL);
+
+  while(1) {
+    ;
+  }
 
   return 0;
 }
